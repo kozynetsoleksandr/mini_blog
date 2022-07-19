f44a087c965a9ec2810e6ef26aaf4b6d34d00b0c
skozinets <kozynetsoleksandr8@gmail.com>
2022-07-20 00:05:18 +0300

    add migrate for article

---
+++ migrations/m220719_205030_create_article_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%article}}`.
+ */
+class m220719_205030_create_article_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%article}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->text(1024)->notNull(),
+            'slug' => $this->text(1024)->notNull(),
+            'body' => $this->text()->notNull(),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+            'created_by' => $this->integer(),
+            'updated_by' => $this->integer(),
+
+        ]);
+
+        $this->addForeignKey(
+            'article_user_created_by_fk',  // это "условное имя" ключа
+            'article', // это название текущей таблицы
+            'created_by', // это имя поля в текущей таблице, которое будет ключом
+            'user', // это имя таблицы, с которой хотим связаться
+            'id' // это поле таблицы, с которым хотим связаться
+        );
+
+        $this->addForeignKey(
+            'article_user_updated_by_fk',  // это "условное имя" ключа
+            'article', // это название текущей таблицы
+            'updated_by', // это имя поля в текущей таблице, которое будет ключом
+            'user', // это имя таблицы, с которой хотим связаться
+            'id' // это поле таблицы, с которым хотим связаться
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%article}}');
+
+        //Добавляем удаление внешнего ключа
+        $this->dropForeignKey(
+            'created_by',
+            'user'
+        );
+
+        $this->dropForeignKey(
+            'updated_by',
+            'user'
+        );
+    }
+}
