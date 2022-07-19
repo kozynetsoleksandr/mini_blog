93548802e65c2db0ab6189a41362374af9e9592d
skozinets <kozynetsoleksandr8@gmail.com>
2022-07-19 21:02:07 +0300

    start project

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'v5MEJhxlz2JHiLjQP-kpjLKx_QJuHUjC',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
